5182562aee7f7f8c9b7f213871f424d4fa74ca5a
paweltheriperr <pawel.programista.kuczmik@gmail.com>
2023-02-19 00:53:53 +0100

    dodanie bazy danych sql do django

--- puzzlechessdb/chessfen/models.py
+++ puzzlechessdb/chessfen/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# Klasa FEN dziedziczy po klasie Model z modułu models.
+class FEN(models.Model):
+    # Pole name typu CharField o maksymalnej długości 100 znaków.
+    name = models.CharField(max_length=100)
+    # Pole fen typu CharField o maksymalnej długości 100 znaków.
+    fen = models.CharField(max_length=100)
