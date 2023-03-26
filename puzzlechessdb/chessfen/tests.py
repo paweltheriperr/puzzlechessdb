c79bd3da3349845715b5c9a63f21c5e2c2b7dfed
paweltheriperr <pawel.programista.kuczmik@gmail.com>
2023-03-26 17:54:57 +0200

    UNIT TEST

--- puzzlechessdb/chessfen/tests.py
+++ puzzlechessdb/chessfen/tests.py
@@ -1,36 +1,20 @@
-from django.test import TestCase
 from django.test import TestCase, Client
 from django.urls import reverse
-from chess import Board
-from .models import Fen
+from django.core.files.uploadedfile import SimpleUploadedFile
 
-class ChessFenTestCase(TestCase):
+class ChessFenIndexViewTestCase(TestCase):
     def setUp(self):
         self.client = Client()
         self.url = reverse('chessfen:index')
+        self.fen_file = SimpleUploadedFile("test_fen.txt", b"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
 
-
-    def test_get_index_page(self):
-        response = self.client.get(self.url)
+    def test_valid_file_upload(self):
+        response = self.client.post(self.url, {'fen_file': self.fen_file})
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'chessfen/index.html')
-        self.assertContains(response, 'FEN')
-#Testowanie wyświetlania strony głównej (GET request)
-    def test_post_invalid_fen(self):
-        data = {'chess_fen': 'invalid fen'}
-        response = self.client.post(self.url, data=data)
-        self.assertTemplateUsed(response, 'chessfen/index.html')
-        self.assertContains(response, 'Niepoprawny FEN.')
-        self.assertEqual(Fen.objects.count(), 0)
-        #Testowanie wprowadzenia niepoprawnego FEN (POST request)
+        self.assertIn('success_message', response.context)
 
-    def test_post_valid_fen(self):
-        board = Board()
-        fen_str = board.fen()
-        data = {'chess_fen': fen_str}
-        response = self.client.post(self.url, data=data)
-        self.assertTemplateUsed(response, 'chessfen/index.html')
-        self.assertContains(response, fen_str)
-        self.assertEqual(Fen.objects.count(), 1)
-        self.assertEqual(Fen.objects.last().fen, fen_str)
-        #Testowanie wprowadzenia poprawnego FEN (POST request)
+    def test_invalid_file_upload(self):
+        invalid_file = SimpleUploadedFile("test_fen.txt", b"invalid_fen_string")
+        response = self.client.post(self.url, {'fen_file': invalid_file})
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('error_message', response.context)
